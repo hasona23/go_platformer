eb7ca847cb5545bb3d8281c163bcf87f7fa97fc3
hasona23 <hassan20302008@gmail.com>
2024-08-17 17:34:57 +0300

    Adjustments to shooting visuals like gun bang effect and jumping

--- entities/player.go
+++ entities/player.go
@@ -37,7 +37,7 @@ func (p *Player) Update(tiles map[[2]int]components.Rect) {
 	}
 	p.Vel.Dir[0] = 0
 	if p.Vel.Dir[1] > 0 && p.isJumping {
-		p.Vel.Dir[1] = min(2, p.Vel.Dir[1]+.8)
+		p.Vel.Dir[1] = min(3, p.Vel.Dir[1]+.8)
 	} else {
 		p.Vel.Dir[1] = min(2, p.Vel.Dir[1]+0.1)
 	}
@@ -65,15 +65,24 @@ func (p *Player) Update(tiles map[[2]int]components.Rect) {
 
 }
 func (p Player) Draw(screen *ebiten.Image, cam components.Camera) {
-	p.PhysicsEntity.Draw(screen, cam)
-	if inpututil.IsKeyJustPressed(ebiten.KeyEnter) || inpututil.IsKeyJustPressed(ebiten.KeyE) {
-		op := &ebiten.DrawImageOptions{}
-		op.GeoM.Translate(float64(p.Pos.Pos[0]+cam.X+7), float64(float64(p.Pos.Pos[1]+cam.Y)-5.5))
-		screen.DrawImage(assets.SpriteSheet.SubImage(image.Rect(3*16, 4*16, 4*16, 5*16)).(*ebiten.Image), op)
-	}
 	for _, b := range p.Bullets {
 		b.Draw(screen, cam)
 	}
+	if inpututil.IsKeyJustPressed(ebiten.KeyEnter) || ebiten.IsKeyPressed(ebiten.KeyE) {
+		op := &ebiten.DrawImageOptions{}
+		if !p.Anim.Flip {
+			op.GeoM.Translate(float64(p.Pos.Pos[0]+cam.X+8), float64(float64(p.Pos.Pos[1]+cam.Y)-5.5))
+		} else {
+
+			p.Anim.Origin(op)
+			op.GeoM.Scale(-1, 1)
+			op.GeoM.Translate(float64(p.Pos.Pos[0]+cam.X-16), float64(float64(p.Pos.Pos[1]+cam.Y))+2)
+
+		}
+		screen.DrawImage(assets.SpriteSheet.SubImage(image.Rect(3*16, 4*16, 4*16, 5*16)).(*ebiten.Image), op)
+	}
+	p.PhysicsEntity.Draw(screen, cam)
+
 }
 func (p *Player) shoot() {
 	if inpututil.IsKeyJustPressed(ebiten.KeyEnter) || inpututil.IsKeyJustPressed(ebiten.KeyE) {
